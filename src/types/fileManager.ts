365865d2291d2749baa60c566eb6a9a61e43597a
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-09-05 12:55:44 -0300

    remove file manager

--- src/types/fileManager.ts
+++
@@ -1,9 +0,0 @@
-import { MonthsType } from './months'
-
-export type FileManagerType = 'receipt' | 'expense' | 'saving'
-
-export type ConfigType = {
-  year: number
-  type: FileManagerType
-  month: MonthsType
-}
