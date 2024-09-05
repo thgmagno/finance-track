7d42f2c8f697150ffa80f446edbc652e3ebfe9b7
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-09-04 22:01:36 -0300

    add fn to crypt and decrypt json

---
+++ src/utils/crypto.ts
@@ -0,0 +1,21 @@
+import crypto from 'crypto'
+
+const algorithm = 'aes-256-cbc'
+
+const key = Buffer.from(process.env.ENCRYPTION_KEY!, 'hex')
+const iv = Buffer.from(process.env.ENCRYPTION_IV!, 'hex')
+
+export const encryptJson = (jsonData: object): string => {
+  const jsonStr = JSON.stringify(jsonData)
+  const cipher = crypto.createCipheriv(algorithm, key, iv)
+  let encrypted = cipher.update(jsonStr, 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+  return encrypted
+}
+
+export const decryptJson = (encryptedData: string): object => {
+  const decipher = crypto.createDecipheriv(algorithm, key, iv)
+  let decrypted = decipher.update(encryptedData, 'hex', 'utf-8')
+  decrypted += decipher.final('utf-8')
+  return JSON.parse(decrypted)
+}
