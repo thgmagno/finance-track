0042499a4b765af737cf391c753418bb0852ea1b
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-09-16 16:14:02 -0300

    update buttons.tsx

--- src/components/common/Buttons.tsx
+++ src/components/common/Buttons.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import clsx from 'clsx'
-import { Loader2 } from 'lucide-react'
+import { Loader } from 'lucide-react'
 import { ButtonHTMLAttributes } from 'react'
 import { useFormStatus } from 'react-dom'
 
@@ -12,7 +11,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function ButtonFormSubmit({ title, className, ...rest }: Props) {
   const { pending } = useFormStatus()
-  const Spinner = () => <Loader2 className="h-5 w-5 animate-spin" />
+  const Spinner = () => <Loader className="h-5 w-5 animate-spin" />
 
   return (
     <Button type="submit" className={className} disabled={pending} {...rest}>
